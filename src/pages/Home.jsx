db1f723b21875e283cc86353fc2f6183f99f53be
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-11-11 18:27:15 +0100

    baggrundsbillede skifter alt efter siden den er på

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import InfoSection from "../components/infoSection/InfoSection";
 import PageHeader from "../components/pageHeader/PageHeader";
 import Reviews from "../components/reviews/Reviews";
+import logo from "/logo.png"
+import bgImg from "../assets/image_00.jpg"
 
 const Home = () => {
   return (
     <article>
-      <PageHeader />
+      <PageHeader
+        logo={logo}
+        title="Gittes"
+        subtitle="Glamping"
+        bgUrl={bgImg}
+      />
       <InfoSection />
       <Reviews />
     </article>
