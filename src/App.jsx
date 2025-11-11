7a8fc872d2e533ef5089bc2086d225529c470cf3
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-11-11 09:53:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useRoutes } from "react-router";
 import "./App.css";
-import Home from "./pages/Home";
-import Stays from "./pages/Stays";
+import Home from "./pages/Home.jsx";
+import Stays from "./pages/Stays.jsx";
 import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/footer/Footer";
 
