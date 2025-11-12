ddf6f65c9955423b347cac54490fffdf7c662727
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-11-12 10:42:18 +0100

    Infoboks content lagt i array og indsættes dynamis

--- src/components/infoSection/InfoSection.jsx
+++ src/components/infoSection/InfoSection.jsx
@@ -2,25 +2,29 @@ import Button from "../button/Button";
 import styles from "./infoSection.module.css";
 import gitte from "../../assets/gitte.jpg";
 
-const InfoSection = () => {
-  return (
-    <section className={styles.infoSection}>
-      <h2>Kom og prøv glamping hos Gitte</h2>
-      <p>
-        Vi er stolte af at byde dig velkommen til Gitte’s Glamping, hvor
-        hjertevarme og omsorg møder naturens skønhed og eventyr. Vores
-        dedikerede team, anført af Gitte selv, er her for at skabe den perfekte
-        ramme om din luksuriøse udendørsoplevelse. Vi stræber efter at skabe
-        minder og fordybelse, uanset om du besøger os som par, familie eller
-        soloeventyrer. Vi tilbyder en bred vifte af aktiviteter og
-        arrangementer, der passer til alle aldre og interesser. Udforsk naturen,
-        slap af ved bålet, del historier med nye venner, eller find indre ro med
-        vores wellnessaktiviteter.
-      </p>
-      <img src={gitte} alt="gitte" />
-      <Button buttonText={"Se vores ophold"} />
-    </section>
-  );
+const InfoContent = {
+  Home: {
+    title: "Kom og prøv glamping hos Gitte",
+    body: "Vi er stolte af at byde dig velkommen til Gitte’s Glamping, hvor hjertevarme og omsorg møder naturens skønhed og eventyr. Voresdedikerede team, anført af Gitte selv, er her for at skabe den perfekte ramme om din luksuriøse udendørsoplevelse. Vi stræber efter at skabe minder og fordybelse, uanset om du besøger os som par, familie eller soloeventyrer. Vi tilbyder en bred vifte af aktiviteter og arrangementer, der passer til alle aldre og interesser. Udforsk naturen, slap af ved bålet, del historier med nye venner, eller find indre ro med vores wellnessaktiviteter.",
+  },
+  Stays: {
+    title: "Vi har ophold til enhver smag",
+    body: "Vores glampingophold er skabt til at tilbyde en kombination af eventyr og afslapning. Det er den ideelle flugt fra byens støj og stress, og det perfekte sted at genoplade batterierne i en naturskøn indstilling. Book dit ophold i dag og giv dig selv lov til at fordybe dig i naturen og nyde luksus i det fri. Vi ser frem tid at byde dig velkommen til en oplevelse fyldt med komfort, eventyr og skønhed.",
+  },
+};
+
+const InfoSection = (page) => {
+
+  const Content = InfoContent[page] || null;
+
+      return (
+        <section className={styles.infoSection}>
+          <h2>{Content.title}</h2>
+          <p>{Content.body}</p>
+          <img src={gitte} alt="gitte" />
+          <Button buttonText={"Se vores ophold"} />
+        </section>
+      );
 };
 
 export default InfoSection;
