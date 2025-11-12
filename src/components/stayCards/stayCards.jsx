c1f9263db64502c2bc5f210d5c79002f353a6621
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-11-12 12:20:51 +0100

    tilføjede imgSrc til stays

--- src/components/stayCards/stayCards.jsx
+++ src/components/stayCards/stayCards.jsx
@@ -5,22 +5,26 @@ import StayCard from "../StayCard/StayCard"
 const StayCards = () => {
 
     const stays = [
-         {
-            title: "Weekendtur",
-            people: "2",
-            price: "4200"
-        },
-         {
-            title: "Familiepakken",
-            people: "3-6",
-            price: "6100"
-        },
-         {
-            title: "Romantisk getaway",
-            people: "2",
-            price: "3500"
-        }
-    ]
+      {
+        title: "Weekendtur",
+        people: "2",
+        price: "4200",
+        imgSrc: "../../assets/stays/weekend.jpg",
+      },
+      {
+        title: "Familiepakken",
+        people: "3-6",
+        price: "6100",
+        imgSrc: "../../assets/stays/familiepakken.jpg",
+      },
+      {
+        title: "Romantisk getaway",
+        people: "2",
+        price: "3500",
+        imgSrc: "../../assets/stays/tentlights.jpg",
+      },
+    ];
+    
 
     return (
 
